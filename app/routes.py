ba6c24c4be092553b89500431239e3d6ad75592b
Victor Senna <vhsenna@gmail.com>
2022-04-15 01:40:43 -0300

    Return complete HTML page from view function

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
+from cgitb import html
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'username': 'João'}
+    return '''
+    <html>
+        <head>
+            <title>Home Page - Microblog</title>
+        </head>
+        <body>
+            <h1>Hello, ''' + user['username'] + '''!</h1>
+        </body>
+    </html>'''
