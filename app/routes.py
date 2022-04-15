f3c6f7c0e740348013d87c655a1d39bd05268ee8
Victor Senna <vhsenna@gmail.com>
2022-04-15 01:58:24 -0300

    Use render_template() function

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,8 @@
-from cgitb import html
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'João'}
-    return '''
-    <html>
-        <head>
-            <title>Home Page - Microblog</title>
-        </head>
-        <body>
-            <h1>Hello, ''' + user['username'] + '''!</h1>
-        </body>
-    </html>'''
+    return render_template('index.html', title='Home', user=user)
