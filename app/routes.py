88f4d759c46c920eda3d24627da0f100eec29918
Victor Senna <vhsenna@gmail.com>
2022-04-15 13:02:16 -0300

    Fake posts in view function

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'João'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'Maria'},
+            'body': 'Beautiful day in Recife!'
+        },
+        {
+            'author': {'username': 'Victor'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
