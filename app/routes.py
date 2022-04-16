7b7a3caf229670cbddac6e2672dc8f0f6279b838
Victor Senna <vhsenna@gmail.com>
2022-04-16 18:17:50 -0300

    User profile view function

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
-from flask import render_template, flash, redirect, url_for, request
+from flask import flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required, login_user, logout_user
+from werkzeug.urls import url_parse
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
-from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
-from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
@@ -57,3 +57,13 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'},
+    ]
+    return render_template('user.html', user=user, posts=posts)
