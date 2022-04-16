e56ea3776b2700468d136edfc2c49edc5dab6289
Victor Senna <vhsenna@gmail.com>
2022-04-16 01:16:06 -0300

    Flask-Login user loader function

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from app import login
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -27,3 +28,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Post {self.body}>'
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
