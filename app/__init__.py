04ed4c03874bec9b9c54b2031263eba37eea92c4
Victor Senna <vhsenna@gmail.com>
2022-04-16 22:58:03 -0300

    Logging to a file

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 import logging
-from logging.handlers import SMTPHandler
+import os
+from logging.handlers import RotatingFileHandler, SMTPHandler
 from config import Config
 from flask import Flask
 from flask_login import LoginManager
@@ -13,7 +14,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models, errors
+from app import errors, models, routes
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -30,3 +31,13 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
