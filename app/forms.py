f1a900409bc35a15a254f0d17aa01762137b3bec
Victor Senna <vhsenna@gmail.com>
2022-04-16 21:26:47 -0300

    Fix typo

--- app/forms.py
+++ app/forms.py
@@ -27,7 +27,7 @@ class RegistrationForm(FlaskForm):
         if user is not None:
             raise ValidationError('Please use a different email address.')
 
-class EditorProfileForm(FlaskForm):
+class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
