36c460c374f237c85177b6dbf6d077f5e4876695
Victor Senna <vhsenna@gmail.com>
2022-04-18 21:45:50 -0300

    Followers association table

--- app/models.py
+++ app/models.py
@@ -4,6 +4,11 @@ from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 from app import db, login
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id')),
+)
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
